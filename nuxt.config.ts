720990c733447f1b5726678282696c0db24078e1
Serhii Olar <olar_ss@groupbwt.com>
2023-06-06 23:47:47 +0300

    hotfix:fix ui showing duplicate buttons and buttons for unauthorized users, give an accept for admin deleting listings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
                     integrity:'sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4',
                     crossorigin:'anonymous',
                 }
-            ]    
+            ],
         },        
     },
     runtimeConfig:{
