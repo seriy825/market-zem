f0ea6939823d8b3f0721ff71f135636d524f41e3
Serhii Olar <olar_ss@groupbwt.com>
2023-06-06 23:47:47 +0300

    hotfix:fix ui showing duplicate buttons and buttons for unauthorized users, give an accept for admin deleting listings

--- store/user.js
+++ store/user.js
@@ -2,30 +2,16 @@ import { useAuthStore } from "./auth";
 import { defineStore } from "pinia";
 
 export const useUserStore = defineStore("user", {
-  state: () => {
-      return {
-          user:{}
-      };
-  },
   actions: {      
-      async fetchUser(){
-        try{          
-          const {token} = useAuthStore();
-          return await $fetch(useRuntimeConfig().public.backendUrl+'/api/user/'+token).then(response=>{
-            this.user=response.user;
-          });
-        }
-        catch(err){
-          console.error(err);
-        }
-      },  
       async updateUser(updatedUser){
         try{          
-          return await $fetch(useRuntimeConfig().public.backendUrl+'/api/user/'+this.user.id,{
+          const {user} = useAuthStore();
+          return await $fetch(useRuntimeConfig().public.backendUrl+'/api/user/'+user.id,{
             method:'POST',
             body:updatedUser,
           }).then(response=>{
-            useAuthStore().user=response;
+            if (response.status===200)
+              useAuthStore().user=response;
           });
         }
         catch(err){
