3ffd32456bc11479fe1987a1424229853968cbad
Serhii Olar <olar_ss@groupbwt.com>
2023-06-06 23:47:47 +0300

    hotfix:fix ui showing duplicate buttons and buttons for unauthorized users, give an accept for admin deleting listings

--- pages/Offers/[id].vue
+++ pages/Offers/[id].vue
@@ -30,19 +30,19 @@
           <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modal">
             Задати питання
           </button>        
-          <button class="btn btn-danger fw-bold mx-sm-2 my-sm-0 my-2" data-bs-toggle="modal" data-bs-target="#delete" v-if="listing.user.id===user.id">
+          <button class="btn btn-danger fw-bold mx-sm-2 my-sm-0 my-2" data-bs-toggle="modal" data-bs-target="#delete" v-if="listing.user.id===user.id||isAdmin">
             Видалити оголошення
           </button>   
         </div>
         <div class="row align-items-center">
           <span class="clickable col" @click="showPhone">{{show?'Сховати номер телефону':'Переглянути номер телефону'}}</span>
           <p class="col">Створено {{ convertDate(listing.created_at) }}</p>
-          <button class="btn col" @click.prevent="removeListingFromFavorites()" v-if="checkFavoriteListing()">
+          <button class="btn col" @click.prevent="removeListingFromFavorites()" v-if="checkFavoriteListing()&&!isAdmin">
             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
             </svg>
           </button>
-          <button class="btn col" @click.prevent="addListingToFavorites()" v-else>
+          <button class="btn col" @click.prevent="addListingToFavorites()" v-else-if="!isAdmin">
             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="" class="bi bi-heart" viewBox="0 0 16 16">
               <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
             </svg>
@@ -118,7 +118,7 @@ import { useAuthStore } from '~/store/auth';
       }
     },
     computed:{
-      ...mapState(useAuthStore,['user']),
+      ...mapState(useAuthStore,['user','isAdmin']),
     },
     async mounted(){
       this.listing = await this.fetchListing(this.$route.params.id);
